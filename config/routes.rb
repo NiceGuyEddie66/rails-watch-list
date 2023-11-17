6156148aabd5f66695c826403fe7217b466d3ca2
Edward Clary <edwardclary@gmail.com>
2023-11-17 11:56:14 +0100

    Heroku tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root to: 'lists#index'
-  resources :lists, only: [:new, :show, :create, :destroy ] do
+  resources :lists, only: [ :new, :show, :create, :destroy ] do
     resources :bookmarks, only: [ :new, :create ]
   end
   resources :bookmarks, only: :destroy
